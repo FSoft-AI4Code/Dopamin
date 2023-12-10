9f75acbe569a39eaec9c9859ee58e489482f0f87
Nam Le Hai <nam.lh173264@gmail.com>
2023-12-11 00:01:40 +0700

    Update custom_model.py

--- training/custom_model.py
+++ training/custom_model.py
@@ -161,45 +161,6 @@ class HSUM(nn.Module):
         avg_logits = torch.sum(torch.stack(logitses), dim=0) / self.config.count
         return avg_logits
 
-class PSUM(nn.Module):
-    def __init__(self, config):
-        super(PSUM, self).__init__()
-        self.config = config
-        self.pre_layers = torch.nn.ModuleList()
-        for i in range(config.count):
-            self.pre_layers.append(RobertaLayer(config))
-
-        self.apply(self._init_weights)
-
-
-
-    def _init_weights(self, module):
-        """Initialize the weights"""
-        if isinstance(module, nn.Linear):
-            # Slightly different from the TF version which uses truncated_normal for initialization
-            # cf https://github.com/pytorch/pytorch/pull/5617
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-        
-    def forward(self, layers, attention_mask, return_list= False):
-        logitses = []
-        output = torch.zeros_like(layers[0])
-        for i in range(self.config.count):
-            logits = self.pre_layers[i](layers[-i-1], attention_mask)[0]
-            logitses.append(logits)
-        if return_list:
-            return logitses
-        avg_logits = torch.sum(torch.stack(logitses), dim=0) / self.config.count
-        return avg_logits
-
 class CodeBERTHSUMForSequenceClassification(RobertaPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
@@ -209,10 +170,8 @@ class CodeBERTHSUMForSequenceClassification(RobertaPreTrainedModel):
         self.roberta = RobertaModel(config, add_pooling_layer=False)
         self.classifier = CustomRobertaClassificationHead(config, 768)
 
-        if self.config.mix_type == 'HSUM':
-            self.mixlayer = HSUM(config)
-        else:
-            self.mixlayer = PSUM(config)
+        self.mixlayer = HSUM(config)
+
         # Initialize weights and apply final processing
         self.post_init()
 
@@ -302,101 +261,3 @@ class CodeBERTHSUMForSequenceClassification(RobertaPreTrainedModel):
             attentions=outputs.attentions,
         )
 
-
-class CodeBERTMixFeaturesForSequenceClassification(RobertaPreTrainedModel):
-    def __init__(self, config):
-        super().__init__(config)
-        self.num_labels = config.num_labels
-        self.config = config
-
-        self.roberta = RobertaModel(config, add_pooling_layer=False)
-        self.classifier = CustomRobertaClassificationHead(config, 4*768)
-
-        self.post_init()
-
-    def forward(
-        self,
-        input_ids: Optional[torch.LongTensor] = None,
-        attention_mask: Optional[torch.FloatTensor] = None,
-        token_type_ids: Optional[torch.LongTensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
-        head_mask: Optional[torch.FloatTensor] = None,
-        inputs_embeds: Optional[torch.FloatTensor] = None,
-        labels: Optional[torch.LongTensor] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-        weighted_loss = None
-    ) -> Union[Tuple[torch.Tensor], SequenceClassifierOutput]:
-        r"""
-        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
-            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
-            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
-            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
-        """
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-        # if labels is not None:
-        output_hidden_states = True
-            
-        outputs = self.roberta(
-            input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            position_ids=position_ids,
-            head_mask=head_mask,
-            inputs_embeds=inputs_embeds,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-        if return_dict:
-            layers = outputs.hidden_states
-        else:
-            layers = outputs[2]
-        # extend_attention_mask = (1.0 - attention_mask[:,None, None, :]) * -10000.0
-
-        # sequence_output = self.mixlayer(layers, extend_attention_mask)
-        sequence_output = torch.cat((layers[-1][:,0, ...],layers[-2][:,0, ...], layers[-3][:,0, ...], layers[-4][:,0, ...]),-1)
-
-        logits = self.classifier(sequence_output)
-
-        loss = None
-        if labels is not None:
-            # move labels to correct device to enable model parallelism
-            labels = labels.to(logits.device)
-            if self.config.problem_type is None:
-                if self.num_labels == 1:
-                    self.config.problem_type = "regression"
-                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
-                    self.config.problem_type = "single_label_classification"
-                else:
-                    self.config.problem_type = "multi_label_classification"
-
-            if self.config.problem_type == "regression":
-                loss_fct = MSELoss()
-                if self.num_labels == 1:
-                    loss = loss_fct(logits.squeeze(), labels.squeeze())
-                else:
-                    loss = loss_fct(logits, labels)
-            elif self.config.problem_type == "single_label_classification":
-                if weighted_loss is not None:      
-                    loss_fct = CrossEntropyLoss(weight=weighted_loss[0][:self.num_labels])
-                else:
-                    loss_fct = CrossEntropyLoss()
-
-                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
-            elif self.config.problem_type == "multi_label_classification":
-                loss_fct = BCEWithLogitsLoss()
-                loss = loss_fct(logits, labels)
-
-        if not return_dict:
-            output = (logits,) + outputs[2:]
-            return ((loss,) + output) if loss is not None else output
-
-        return SequenceClassifierOutput(
-            loss=loss,
-            logits=logits,
-            hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions,
-        )
